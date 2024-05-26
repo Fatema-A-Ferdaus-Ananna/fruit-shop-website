48691717d6e3a3bc509f1daef5956d3b8d217d6b
Fatema-A-Ferdaus <fatema.a.ferdaus@gmail.com>
2024-05-26 18:41:13 +0600

    hide menu bar

--- fruitJuiceShopWebsite/script/landingPageScript.js
+++ fruitJuiceShopWebsite/script/landingPageScript.js
@@ -4,6 +4,10 @@ const register_btn = document.querySelector(".register");
 const cancel_signUp_btn = document.querySelector("#cancel-signUp");
 const cancel_signIn_btn = document.querySelector("#cancel-signIn");
 
+const menubar_btn = document.querySelector("#menubar-btn");
+const hide_menubar_btn = document.querySelector("#hide-menubar-btn");
+const hide_menubar = document.querySelector(".nav-menu");
+
 const login_form = document.querySelector(".container-signIn");
 const signUp_form = document.querySelector(".container-signUp");
 
@@ -30,3 +34,12 @@ cancel_signIn_btn.addEventListener("click", cancel_signIn);
 
 register_btn.addEventListener("click", showSignupForm);
 cancel_signUp_btn.addEventListener("click", cancel_signUp);
+
+menubar_btn.addEventListener("click", () => {
+  menubar_btn.style.display = "none";
+  hide_menubar.style.display = "flex";
+});
+hide_menubar_btn.addEventListener("click", () => {
+  menubar_btn.style.display = "flex";
+  hide_menubar.style.display = "none";
+});
