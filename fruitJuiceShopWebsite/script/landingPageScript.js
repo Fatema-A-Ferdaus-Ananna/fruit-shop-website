1b048e2cd39d68e91d3a8abf41dffd0ff2609775
Fatema-A-Ferdaus <fatema.a.ferdaus@gmail.com>
2024-05-26 11:23:49 +0600

    added registration form

--- fruitJuiceShopWebsite/script/landingPageScript.js
+++ fruitJuiceShopWebsite/script/landingPageScript.js
@@ -1,15 +1,21 @@
 const login_btn = document.querySelector(".login");
 const register_btn = document.querySelector(".register");
+const cancel_btn = document.querySelector(".cancel-btn");
 
-const showLoginForm = () => {
-  const login_form = document.querySelector(".container-signIn-signUp");
-  login_form.style.display = "block";
-};
+// const showLoginForm = () => {
+//   const login_form = document.querySelector(".container-signIn-signUp");
+//   login_form.style.display = "block";
+// };
 
+const signUp_form = document.querySelector(".container-signIn-signUp");
 const showSignupForm = () => {
-  const signUp_form = document.querySelector(".container-signIn-signUp .register-container");
-  signUp_form.style.display = "block";
+  signUp_form.style.display = "flex";
 };
 
-login_btn.addEventListener("click", showLoginForm);
-register_btn_btn.addEventListener("click", showSignupForm);
+const cancelSignupForm = () => {
+  signUp_form.style.display = "none";
+};
+
+// login_btn.addEventListener("click", showLoginForm);
+register_btn.addEventListener("click", showSignupForm);
+cancel_btn.addEventListener("click", cancelSignupForm);
