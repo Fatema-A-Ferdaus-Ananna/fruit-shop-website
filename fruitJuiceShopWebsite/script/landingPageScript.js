181f351b12363d21399cb3fb8e9407252b823db8
Fatema-A-Ferdaus <fatema.a.ferdaus@gmail.com>
2024-05-26 15:04:59 +0600

    added login form

--- fruitJuiceShopWebsite/script/landingPageScript.js
+++ fruitJuiceShopWebsite/script/landingPageScript.js
@@ -1,21 +1,32 @@
 const login_btn = document.querySelector(".login");
 const register_btn = document.querySelector(".register");
-const cancel_btn = document.querySelector(".cancel-btn");
 
-// const showLoginForm = () => {
-//   const login_form = document.querySelector(".container-signIn-signUp");
-//   login_form.style.display = "block";
-// };
+const cancel_signUp_btn = document.querySelector("#cancel-signUp");
+const cancel_signIn_btn = document.querySelector("#cancel-signIn");
 
-const signUp_form = document.querySelector(".container-signIn-signUp");
+const login_form = document.querySelector(".container-signIn");
+const signUp_form = document.querySelector(".container-signUp");
+
+const showLoginForm = () => {
+  signUp_form.style.display = "none";
+  login_form.style.display = "flex";
+};
+
 const showSignupForm = () => {
+  login_form.style.display = "none";
   signUp_form.style.display = "flex";
 };
 
-const cancelSignupForm = () => {
+const cancel_signIn = () => {
+  login_form.style.display = "none";
+};
+
+const cancel_signUp = () => {
   signUp_form.style.display = "none";
 };
 
-// login_btn.addEventListener("click", showLoginForm);
+login_btn.addEventListener("click", showLoginForm);
+cancel_signIn_btn.addEventListener("click", cancel_signIn);
+
 register_btn.addEventListener("click", showSignupForm);
-cancel_btn.addEventListener("click", cancelSignupForm);
+cancel_signUp_btn.addEventListener("click", cancel_signUp);
